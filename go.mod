613fef3876d76b6874a3840900b08bed8675ce7e
Sean Liao <seankhliao@gmail.com>
2020-09-20 23:32:44 +0200

    logging to stream

--- go.mod
+++ go.mod
@@ -5,5 +5,7 @@ go 1.14
 require (
 	github.com/rs/zerolog v1.18.0
 	go.opentelemetry.io/otel v0.11.0
+	go.seankhliao.com/stream v0.1.0
 	go.seankhliao.com/usvc v0.4.0
+	google.golang.org/grpc v1.32.0
 )
--- main.go
+++ main.go
@@ -2,15 +2,19 @@ package main
 
 import (
 	"context"
+	"flag"
 	"net/http"
 	"os"
 	"strings"
 	"text/template"
+	"time"
 
 	"github.com/rs/zerolog"
 	"go.opentelemetry.io/otel/api/global"
 	"go.opentelemetry.io/otel/api/metric"
+	"go.seankhliao.com/stream"
 	"go.seankhliao.com/usvc"
+	"google.golang.org/grpc"
 )
 
 //go:generate go run generate.go template.gohtml
@@ -22,7 +26,7 @@ const (
 func main() {
 	var s Server
 
-	srvc := usvc.DefaultConf()
+	srvc := usvc.DefaultConf(&s)
 	s.log = srvc.Logger()
 
 	s.module = metric.Must(global.Meter(os.Args[0])).NewInt64Counter(
@@ -32,10 +36,17 @@ func main() {
 
 	s.tmpl = template.Must(template.New("page").Parse(tmplStr))
 
+	cc, err := grpc.Dial(s.streamAddr)
+	if err != nil {
+		s.log.Error().Err(err).Msg("connect to stream")
+	}
+	defer cc.Close()
+	s.client = stream.NewStreamClient(cc)
+
 	m := http.NewServeMux()
 	m.Handle("/", s)
 
-	err := srvc.RunHTTP(context.Background(), m)
+	err = srvc.RunHTTP(context.Background(), m)
 	if err != nil {
 		s.log.Fatal().Err(err).Msg("run server")
 	}
@@ -43,7 +54,9 @@ func main() {
 
 type Server struct {
 	// config
-	tmpl *template.Template
+	tmpl       *template.Template
+	streamAddr string
+	client     stream.StreamClient
 
 	// metrics
 	module metric.Int64Counter
@@ -51,7 +64,16 @@ type Server struct {
 	log zerolog.Logger
 }
 
+func (s *Server) RegisterFlags(fs *flag.FlagSet) {
+	fs.StringVar(&s.streamAddr, "stream.addr", "stream:80", "url to connect to stream")
+}
+
 func (s Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+	remote := r.Header.Get("x-forwarded-for")
+	if remote == "" {
+		remote = r.RemoteAddr
+	}
 
 	// filter paths
 	if r.URL.Path == "/" {
@@ -65,5 +87,20 @@ func (s Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		s.log.Error().Err(err).Msg("execute template")
 		w.WriteHeader(http.StatusInternalServerError)
 		return
+	}
+
+	httpRequest := &stream.HTTPRequest{
+		Timestamp: time.Now().Format(time.RFC3339),
+		Method:    r.Method,
+		Domain:    r.Host,
+		Path:      r.URL.Path,
+		Remote:    remote,
+		UserAgent: r.UserAgent(),
+		Referrer:  r.Referer(),
+	}
+
+	_, err = s.client.LogHTTP(ctx, httpRequest)
+	if err != nil {
+		s.log.Error().Err(err).Msg("write to stream")
 	}
 }
